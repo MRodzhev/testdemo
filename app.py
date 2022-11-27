4632fc3585243eaa6264957329e8dace86a8e579
MRodzhev <106818714+MRodzhev@users.noreply.github.com>
2022-11-28 01:28:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import random
 app = Flask(__name__)
 # list of cat images
 images = [
-  "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-  "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
+  "https://tenor.com/view/drei-speed-wette-drei%C3%B6sterreich-drei-test-gif-15189540",
+  "https://tenor.com/view/jack-russell-jack-russell-terrier-jack-russell-gif-13114603",
   "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif"
 ]
 @app.route('/')
